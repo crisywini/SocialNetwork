a4dbe4eb1196aaff5bef41f7159d855eb8750309
Cristian Sanchez <harmaharcri.cs@gmail.com>
2019-10-30 21:18:55 -0500

    MODIFICACION DE CLASES POST, LIKE Y LOVE
    Se realizo el ultimo requisito de dar like y el plus de nuestro equipo que es dar dos likes significa dar love

--- SocialNetwork/src/prueba/pruebas.java
+++ SocialNetwork/src/prueba/pruebas.java
@@ -1,14 +1,6 @@
 package prueba;
 
-import co.uniquindio.redSocial.util.LinkedList;
-
 public class pruebas {
 	public static void main(String[] args) throws Exception {
-		LinkedList<String> lista = new LinkedList<String>();
-		lista.addLast("Liss");
-	
-		System.out.println(lista.toString());
-		lista.remove("Liss");
-		System.out.println(lista.toString());
 	}
 }
